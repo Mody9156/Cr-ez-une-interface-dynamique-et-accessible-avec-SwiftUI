5281f6f0e98d35c8d63b65b10ad922f0a1c58ecb
Modibo KEITA <modykeita2@gmail.com>
2024-10-10 16:22:52 +0200

    created new view

---
+++ JoieFull/JoieFull/View/ArticleListView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleListView.swift
+//  JoieFull
+//
+//  Created by KEITA on 10/10/2024.
+//
+
+import SwiftUI
+
+struct ArticleListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleListView()
+    }
+}
