ff5e07f358e5eaad7d75953ad7d1ce055c293a35
Modibo KEITA <modykeita2@gmail.com>
2024-10-10 17:20:55 +0200

    refactor name

--- JoieFull/JoieFull/ViewModel/ArticleListViewModel.swift
+++ JoieFull/JoieFull/ViewModel/ArticleListViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class ArticleListViewModel : ObservableObject {
-    @Published var articles : [ArticleCatalog] = Service().load("clothes")
+    @Published var articleCatalog : [ArticleCatalog] = Service().load("clothes")
     
     
 }
