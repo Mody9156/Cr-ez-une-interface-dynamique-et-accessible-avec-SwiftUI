f9dce5fb5ce43870c03e13a79d42c3e9034b176e
Modibo KEITA <modykeita2@gmail.com>
2024-10-10 16:43:57 +0200

    created new file

---
+++ JoieFull/JoieFull/Model/ArticleCatalog.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleCatalog.swift
+//  JoieFull
+//
+//  Created by KEITA on 10/10/2024.
+//
+
+import Foundation
+
+struct ArticleCatalog : Identifiable,Decodable {
+    let id = UUID()
+    
+}
---
+++ JoieFull/JoieFull/ViewModel/ArticleListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleListViewModel.swift
+//  JoieFull
+//
+//  Created by KEITA on 10/10/2024.
+//
+
+import Foundation
+
+class ArticleListViewModel : ObservableObject {
+
+    
+    
+}
