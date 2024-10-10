9ca07e027f2b3203e8d4d2b6861c73d60d6dd6aa
Modibo KEITA <modykeita2@gmail.com>
2024-10-10 16:14:42 +0200

    création d'un nouveau dossier

---
+++ JoieFull/JoieFull/JoieFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoieFullApp.swift
+//  JoieFull
+//
+//  Created by KEITA on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoieFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
