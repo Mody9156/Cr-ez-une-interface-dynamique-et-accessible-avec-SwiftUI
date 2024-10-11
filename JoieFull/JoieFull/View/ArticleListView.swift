c29b42dfe206dffdec2668aba1f97b54d7b268c3
Modibo KEITA <modykeita2@gmail.com>
2024-10-11 12:48:44 +0200

    add new scollview

--- JoieFull/JoieFull/View/ArticleListView.swift
+++ JoieFull/JoieFull/View/ArticleListView.swift
@@ -11,21 +11,34 @@ struct ArticleListView: View {
     @ObservedObject var articleListViewModel : ArticleListViewModel
     
     var body: some View {
+        
         NavigationStack {
-            ScrollView {
+            ScrollView([.horizontal]) {
                     LazyHStack(spacing: 10) {
                         ForEach(articleListViewModel.articleCatalog,id: \.name) { article  in
                             if article.category == "TOPS" {
                                 Section{
                                     AsyncImage(url: URL(string: article.picture.url)){ image in
-                                        image.resizable()
-                                        
+                                        image.resizable().cornerRadius(10)
                                         
                                     }placeholder:{
                                         ProgressView()
-                                    }.frame(width: 50, height: 50)
+                                    }.frame(width: 200, height: 200)
                                         
+                                }.navigationBarTitle("Hauts")
+                                        
+                                 
                                 }
+                            if article.category == "BOTTOMS" {
+                                Section{
+                                    AsyncImage(url: URL(string: article.picture.url)){ image in
+                                        image.resizable().cornerRadius(10)
+                                        
+                                    }placeholder:{
+                                        ProgressView()
+                                    }.frame(width: 200, height: 200)
+                                        
+                                }.navigationBarTitle("Bas")
                                         
                                  
                                 }
