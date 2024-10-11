60e0b8b94c7b852528b5c0908e34cf309a98b946
Modibo KEITA <modykeita2@gmail.com>
2024-10-11 15:08:43 +0200

    add extracteur

--- JoieFull/JoieFull/View/ArticleListView.swift
+++ JoieFull/JoieFull/View/ArticleListView.swift
@@ -8,45 +8,40 @@
 import SwiftUI
 
 struct ArticleListView: View {
-    @ObservedObject var articleListViewModel : ArticleListViewModel
+    @ObservedObject var articleListViewModel: ArticleListViewModel
     
     var body: some View {
-        
         NavigationStack {
-            ScrollView([.horizontal]) {
-                    LazyHStack(spacing: 10) {
-                        ForEach(articleListViewModel.articleCatalog,id: \.name) { article  in
-                            if article.category == "TOPS" {
-                                Section{
-                                    AsyncImage(url: URL(string: article.picture.url)){ image in
-                                        image.resizable().cornerRadius(10)
-                                        
-                                    }placeholder:{
-                                        ProgressView()
-                                    }.frame(width: 200, height: 200)
-                                        
-                                }.navigationBarTitle("Hauts")
-                                        
-                                 
-                                }
-                            if article.category == "BOTTOMS" {
-                                Section{
-                                    AsyncImage(url: URL(string: article.picture.url)){ image in
-                                        image.resizable().cornerRadius(10)
-                                        
-                                    }placeholder:{
-                                        ProgressView()
-                                    }.frame(width: 200, height: 200)
-                                        
-                                }.navigationBarTitle("Bas")
-                                        
-                                 
-                                }
-                        }
+            ScrollView {
+                LazyHStack(spacing: 10) {
+                    ForEach(articleListViewModel.articleCatalog, id: \.name){ article in
+                        ArticleView(article: article)
                     }
+                    .navigationTitle("Articles")
+                    .padding()
+                }
             }
         }
-        }
-
+    }
 }
 
+struct ArticleView: View {
+     var article : ArticleCatalog
+    var body: some View {
+       
+            if article.category == "TOPS" || article.category == "BOTTOMS" {
+                VStack {
+                    AsyncImage(url: URL(string: article.picture.url)) { image in
+                        image
+                            .resizable()
+                            .cornerRadius(10)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 200, height: 200)
+                }
+                .padding()
+        
+        }
+    }
+}
