9d7c41de95dcb40e7d2723015c3cbbafc55762e6
Modibo KEITA <modykeita2@gmail.com>
2024-10-14 14:47:32 +0200

    update final class

---
+++ JoieFull/JoieFull/Model/Protocols/URLSessionHTTPClient.swift
@@ -0,0 +1,34 @@
+//
+//  URLSessionHTTPClient.swift
+//  JoieFull
+//
+//  Created by KEITA on 14/10/2024.
+//
+
+import Foundation
+
+final class URLSessionHTTPClient : HTTPService {
+    
+    private let session : URLSession
+    
+    init(session : URLSession = URLSession.shared){
+        self.session = session
+    }
+    
+    private enum AuthenticationFailure: Swift.Error {
+           case invalidRequest
+       }
+    
+    func request(_ request: URLRequest) async throws -> (Data, HTTPURLResponse) {
+        
+        let (data,response) = try await session.data(for:request )
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            
+            throw AuthenticationFailure.invalidRequest
+        }
+    }
+    
+ 
+    
+}
--- JoieFull/JoieFull/Model/URLSessionHTTPClient.swift
+++
@@ -1,8 +0,0 @@
-//
-//  URLSessionHTTPClient.swift
-//  JoieFull
-//
-//  Created by KEITA on 14/10/2024.
-//
-
-import Foundation
