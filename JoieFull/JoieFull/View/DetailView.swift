5580d9aac17c670929363ecffd5c2265da72f210
Modibo KEITA <modykeita2@gmail.com>
2024-10-14 09:34:50 +0200

    update Text

--- JoieFull/JoieFull/View/DetailView.swift
+++ JoieFull/JoieFull/View/DetailView.swift
@@ -40,46 +40,49 @@ struct DetailView: View {
                                         20.92,height: 20.92,widthFrame: 90,heightFrame: 40).padding([.bottom, .trailing], 30)
                      
                     }
-                    VStack {
-                        HStack{
-                            VStack(alignment: .leading) {
-                                Text(article.name).font(.system(size: 14))
-                                    .fontWeight(.semibold)
-                                    .lineSpacing(2.71)
-                                    .multilineTextAlignment(.leading)
-                                Text("\(article.price, format: .number.rounded(increment: 10.0))€").font(.system(size: 14))
-                                    .fontWeight(.regular).lineSpacing(2.71)
-                                    .multilineTextAlignment(.leading)
-                            }
-                            
-                            Spacer()
-                            
-                            VStack(alignment: .trailing) {
-                                HStack {
-                                    Image(systemName: "star.fill").foregroundColor(.yellow)
-                                    if let article = article.likes {
-                                        
-                                        Text("\(article)")
-                                            .font(.system(size: 14))
-                                            .fontWeight(.semibold)
-                                            .lineSpacing(2.71)
-                                            .multilineTextAlignment(.leading)
-                                    }
+                    Section {
+                        VStack {
+                            HStack{
+                                VStack(alignment: .leading) {
+                                    Text(article.name).font(.system(size: 14))
+                                        .fontWeight(.semibold)
+                                        .lineSpacing(2.71)
+                                        .multilineTextAlignment(.leading)
+                                    Text("\(article.price, format: .number.rounded(increment: 10.0))€").font(.system(size: 14))
+                                        .fontWeight(.regular).lineSpacing(2.71)
+                                        .multilineTextAlignment(.leading)
                                 }
                                 
+                                Spacer()
                                 
-                                Text("\(article.original_price, format: .number.rounded(increment: 10.0))€").strikethrough().font(.system(size: 14))
-                                    .fontWeight(.regular)
-                                    .lineSpacing(2.71)
-                                    .multilineTextAlignment(.leading).foregroundColor(.gray)
-                                
-                            }
+                                VStack(alignment: .trailing) {
+                                    HStack {
+                                        Image(systemName: "star.fill").foregroundColor(.yellow)
+                                        if let article = article.likes {
+                                            
+                                            Text("\(article)")
+                                                .font(.system(size: 14))
+                                                .fontWeight(.semibold)
+                                                .lineSpacing(2.71)
+                                                .multilineTextAlignment(.leading)
+                                        }
+                                    }
+                                    
+                                    
+                                    Text("\(article.original_price, format: .number.rounded(increment: 10.0))€").strikethrough().font(.system(size: 14))
+                                        .fontWeight(.regular)
+                                        .lineSpacing(2.71)
+                                        .multilineTextAlignment(.leading).foregroundColor(.gray)
+                                    
+                                }
+                            }.padding()
+                            Text(article.picture.description)
+                                .font(.custom("SF Pro", size: 14))
+                                .fontWeight(.regular)
+                                .font(.largeTitle)
+                                .multilineTextAlignment(.leading)
+                               
                         }.padding()
-                        Text(article.picture.description)
-                            .font(.custom("SF Pro", size: 14))
-                            .fontWeight(.regular)
-                            .lineSpacing(16.71 - 14)
-                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
                        
                 }
