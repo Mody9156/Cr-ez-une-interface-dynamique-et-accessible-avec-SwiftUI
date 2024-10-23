a3a983e1fc99927238f21dff79354d7981339c3d
Modibo KEITA <modykeita2@gmail.com>
2024-10-23 18:50:56 +0200

    add new function

--- JoieFull/JoieFullTests/Model/CatalogProductTests.swift
+++ JoieFull/JoieFullTests/Model/CatalogProductTests.swift
@@ -178,22 +178,6 @@ final class CatalogProductTests: XCTestCase {
     
     
     
-    class MockNetworkService : HTTPService {
-        
-        var mockData: Data?
-        var mockResponse: HTTPURLResponse?
-        var mockError: Error?
-        
-        // Mock de la méthode request
-        func request(_ request: URLRequest) async throws -> (Data, HTTPURLResponse) {
-            if let data = mockData, let response = mockResponse {
-                return (data, response)
-            } else {
-                throw URLError(.badServerResponse)  // Simuler une erreur si les données ou la réponse sont manquantes
-            }
-            
-        }
-    }
     
     
     
--- JoieFull/JoieFullTests/ViewModel/ArticlesViewModelTests.swift
+++ JoieFull/JoieFullTests/ViewModel/ArticlesViewModelTests.swift
@@ -12,27 +12,109 @@ import SwiftUI
 final class ArticlesViewModelTests: XCTestCase {
 
 
-    func testExample() throws {
-        let articleListViewModel = ArticleListViewModel(catalogProduct: CatalogProduct(httpService: MockHTTPService))
-    }
+    func testWhenLoadArticlesNoThrowsError() async throws {
+        let mockNetworkService = MockNetworkService()
+        let mockData =  """
+        [
+            {
+                "id": 1,
+                "picture": {
+                    "url": "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/img/accessories/4.jpg",
+                    "description": "Sac à bandoulière en cuir noir"
+                },
+                "name": "Sac à bandoulière",
+                "category": "ACCESSORIES",
+                "likes": 75,
+                "price": 89.99,
+                "original_price": 99.99
+            },
+           {
+               "id": 2,
+               "picture": {
+                   "url": "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/img/accessories/3.jpg",
+                   "description": "Ceinture en cuir noir élégante"
+               },
+               "name": "Ceinture en cuir",
+               "category": "ACCESSORIES",
+               "likes": 30,
+               "price": 35.00,
+               "original_price": 40.00
+           }
 
-
-    class MockHTTPService: HTTPService {
-        var mockData: Data?
-        var mockResponse: HTTPURLResponse?
-        var mockError: Error?
+        ]
+        """.data(using: .utf8)!
         
-        func request(_ request: URLRequest) async throws -> (Data, HTTPURLResponse) {
-            if let error = mockError {
-                throw error
-            }
-            
-            guard let data = mockData, let response = mockResponse else {
-                throw URLError(.badServerResponse)
-            }
-            
-            return (data, response)
+        mockNetworkService.mockData = mockData
+        let mockResponse = HTTPURLResponse(url: URL(string:"https://exemple.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        mockResponse.method(for: Selector(("GET")))
+        mockNetworkService.mockResponse = mockResponse
+        
+        let articleListViewModel = ArticleListViewModel(catalogProduct: CatalogProduct(httpService: mockNetworkService))
+        
+        do{
+           let loadArticles =  try await articleListViewModel.loadArticles()
+            XCTAssertNotNil(loadArticles)
+            print("function passé")
+        }catch let error {
+            print("Error")
+            XCTAssertEqual(error as? ArticleListViewModel.ArticleListViewModelError, .loadArticlesError)
         }
     }
+    func testWhenLoadArticlesThrowsError() async throws {
+        let mockNetworkService = MockNetworkService()
+        
+        let articleListViewModel = ArticleListViewModel(catalogProduct: CatalogProduct(httpService: mockNetworkService))
+        
+        do{
+           let loadArticles =  try await articleListViewModel.loadArticles()
+            XCTAssertNotNil(loadArticles)
+        }catch let error {
+            XCTAssertEqual(error as? ArticleListViewModel.ArticleListViewModelError, .loadArticlesError)
+        }
+    }
+    func testWhenReloadArticlesNotThrowsErrors()async throws {
 
+        let mockNetworkService = MockNetworkService()
+        let mockData =  """
+        [
+            {
+                "id": 1,
+                "picture": {
+                    "url": "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/img/accessories/4.jpg",
+                    "description": "Sac à bandoulière en cuir noir"
+                },
+                "name": "Sac à bandoulière",
+                "category": "ACCESSORIES",
+                "likes": 75,
+                "price": 89.99,
+                "original_price": 99.99
+            },
+           {
+               "id": 2,
+               "picture": {
+                   "url": "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/img/accessories/3.jpg",
+                   "description": "Ceinture en cuir noir élégante"
+               },
+               "name": "Ceinture en cuir",
+               "category": "ACCESSORIES",
+               "likes": 30,
+               "price": 35.00,
+               "original_price": 40.00
+           }
+
+        ]
+        """.data(using: .utf8)!
+        
+        mockNetworkService.mockData = mockData
+        let mockResponse = HTTPURLResponse(url: URL(string:"https://exemple.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        mockResponse.method(for: Selector(("GET")))
+        mockNetworkService.mockResponse = mockResponse
+        
+        let articleListViewModel = ArticleListViewModel(catalogProduct: CatalogProduct(httpService: mockNetworkService))
+        
+        let reloadArticles: () = try await articleListViewModel.reloadArticles()
+        XCTAssertNotNil(reloadArticles)
+    }
+
+   
 }
